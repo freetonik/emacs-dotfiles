81ad1d3326334064f9f249d6945dc42e29c45dbb
Rakhim Davletkaliyev <git@rakhim.org>
2018-08-02 18:40:24 +0300

    initial config

---
+++ init.el
@@ -0,0 +1,9 @@
+(require 'org)
+;; Open the configuration
+(find-file (concat user-emacs-directory "init.org"))
+;; tangle it
+(org-babel-tangle)
+;; load it
+(load-file (concat user-emacs-directory "init.el"))
+;; finally byte-compile it
+(byte-compile-file (concat user-emacs-directory "init.el"))
