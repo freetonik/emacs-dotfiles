8035bc925642acd87620f2cf62e8bea32835ee72
Rakhim Davletkaliyev <rakhim@rakhim.org>
2019-12-19 19:27:15 +0200

    fixes

--- custom.el
+++ custom.el
@@ -7,14 +7,14 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(markdown-fontify-code-blocks-natively nil)
+ '(markdown-fontify-code-blocks-natively t)
  '(org-agenda-files
    (quote
     ("~/Google Drive/Knowledgebase/org/journal.org" "~/.emacs.d/init.org" "/Users/rakhim/Google Drive/Knowledgebase/org/archive.org" "/Users/rakhim/Google Drive/Knowledgebase/org/links.org" "/Users/rakhim/Google Drive/Knowledgebase/org/main.org")))
  '(org-export-backends (quote (ascii html latex md hugo slimhtml)))
  '(package-selected-packages
    (quote
-    (ox-latex winner-mode esup go-mode ox-tufte smartparens writegood-mode elixir-mode synosaurus ledger-mode ledger embrace change-inner avy edit-indirect ox-pandoc slim-mode clj-refactor ox-slimhtml handlebars-mode org-make-toc ox-hugo-auto-export emmet-mode shackle neotree define-word yaml-mode which-key web-mode visual-regexp use-package spaceline smex simpleclip shell-pop powerthesaurus ox-hugo multiple-cursors move-text markdown-mode magit haml-mode flx expand-region exec-path-from-shell dumb-jump company cider)))
+    (ox-latex winner-mode esup go-mode ox-tufte elixir-mode synosaurus ledger embrace avy edit-indirect ox-pandoc slim-mode clj-refactor ox-slimhtml handlebars-mode org-make-toc ox-hugo-auto-export emmet-mode shackle define-word yaml-mode which-key web-mode use-package spaceline smex simpleclip powerthesaurus ox-hugo multiple-cursors move-text markdown-mode magit haml-mode flx expand-region exec-path-from-shell cider)))
  '(pop-up-frames nil)
  '(safe-local-variable-values
    (quote
@@ -31,7 +31,7 @@
     ("ansi-term" "*ansi-term*"
      (lambda nil
        (ansi-term shell-pop-term-shell)))))
- '(shell-pop-universal-key "s-=")
+ '(shell-pop-universal-key nil)
  '(synosaurus-choose-method (quote default))
  '(vc-follow-symlinks t)
  '(word-wrap t)
