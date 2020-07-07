7dc7590b6af7a0f60f806d7e37efa323df733407
Rakhim Davletkaliyev <contact@rakhim.org>
2020-07-07 12:30:50 +0300

    do not use fixed pitch font for code in md

--- custom.el
+++ custom.el
@@ -25,4 +25,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-code-face ((t nil))))
